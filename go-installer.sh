ae4428a30fc4a29dc174e14fc0a22d444ee99a2b
yyh-gl <yhonda.95.gl@gmail.com>
2022-02-07 21:49:20 +0900

    権限付与処理を汎用化

--- go-installer.sh
+++ go-installer.sh
@@ -4,6 +4,6 @@ curl -OL https://golang.org/dl/go"${VERSION}".darwin-amd64.pkg && \
 open go"${VERSION}".darwin-amd64.pkg && \
 read -rp "インストーラの処理が終わり次第ENTERを押して処理を進める: " && \
 sudo mv /usr/local/go ~/workspaces/Go/"${VERSION}" && \
-sudo chown -hR yyh-gl:staff ~/workspaces/Go/"${VERSION}" && \
+sudo chown -hR `logname`:staff ~/workspaces/Go/"${VERSION}" && \
 rm -rf ./go"${VERSION}".darwin-amd64.pkg && \
 echo "GOPATHの変更＆ターミナルの再起動をお忘れなく"
