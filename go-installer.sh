20112c61137a29b89e2142c2eccb89e56389f27b
yyh-gl <yhonda.95.gl@gmail.com>
2025-04-06 15:01:15 +0900

    Fix lint

--- go-installer.sh
+++ go-installer.sh
@@ -2,11 +2,11 @@
 
 if [ -z "${VERSION}" ]; then echo 'VERSIONを指定してください。'; exit 1; fi
 
-mkdir $HOME/go
+mkdir "$HOME"/go
 curl -OL https://golang.org/dl/go"${VERSION}".darwin-amd64.pkg
 open go"${VERSION}".darwin-amd64.pkg
 read STDIN'?インストーラの処理が終わり次第ENTERを押して処理を進める: '
-sudo mv /usr/local/go $HOME/go/"${VERSION}"
-sudo chown -hR `logname`:staff $HOME/go/"${VERSION}"
+sudo mv /usr/local/go "$HOME"/go/"${VERSION}"
+sudo chown -hR "$(logname)":staff "$HOME"/go/"${VERSION}"
 rm -rf ./go"${VERSION}".darwin-amd64.pkg
 echo "GOPATHの変更＆ターミナルの再起動をお忘れなく"
