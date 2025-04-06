59b95ea64391c9af98df8d3913877b44e7d5518a
yyh-gl <yhonda.95.gl@gmail.com>
2025-04-06 21:50:32 +0900

    Add -p option to mkdir

--- go-installer.sh
+++ go-installer.sh
@@ -2,7 +2,7 @@
 
 if [ -z "${VERSION}" ]; then echo 'VERSIONを指定してください。'; exit 1; fi
 
-mkdir "$HOME"/go
+mkdir -p "$HOME"/go
 curl -OL https://golang.org/dl/go"${VERSION}".darwin-amd64.pkg
 open go"${VERSION}".darwin-amd64.pkg
 read STDIN'?インストーラの処理が終わり次第ENTERを押して処理を進める: '
